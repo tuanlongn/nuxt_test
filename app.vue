2c16d11a4af621635dcbfbafcadca4d5a225d62c
tuanlongn <tuanlongn@gmail.com>
2022-09-29 14:50:37 +0700

    Add page

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <nav>
+      <ul>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+      </ul>
+    </nav>
+
+    <NuxtPage />
   </div>
 </template>
